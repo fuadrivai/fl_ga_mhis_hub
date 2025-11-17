7116e4ffd4a8b63ad84ec980d4e324ea1ae1fa61
Fuad Rivai <fuad.strummer@gmail.com>
2025-11-17 16:27:02 +0700

    cleaning

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -3,9 +3,9 @@ import 'package:fl_ga_mhis_hub/injector/injector.dart';
 import 'package:fl_ga_mhis_hub/service/restclient.dart';
 
 class Api {
-  static const String baseUrl = "http://192.168.204.35:3000/api/";
+  // static const String baseUrl = "http://192.168.204.35:3000/api/";
   // static const String baseUrl = "http://192.168.1.8:3000/api/";
-  // static const String baseUrl = "https://mhis-hub.mhis.link/api/";
+  static const String baseUrl = "https://mhis-hub.mhis.link/api/";
 
   static restClient({Map<String, dynamic>? params, String? baseurl}) async {
     final dio = Dio();
